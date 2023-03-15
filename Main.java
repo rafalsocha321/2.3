a3d66c3959fc5c5b3eaf84a5d370c1fd918c2609
rafalsocha321 <126696041+rafalsocha321@users.noreply.github.com>
2023-03-15 15:25:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+
+    int suma=0;
+      
+    for(int i=0; i<=100; i=i+2){
+      System.out.println(i+", ");
+      suma=suma+i;
+    }
+
+    System.out.println("Suma wszystkich liczb wynosi           "+suma);
+    
+    
+  }
+}
